bbe601b721e15adf7f21d6a6b4c43ada3b50b568
Ermolai <ermolay9@gmail.com>
2023-12-28 01:30:12 +0200

    bot project

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('hero-section').style.animation = 'slideUp 1s ease-out';
+    }, 0); 
+
+    setTimeout(function() {
+        document.getElementById('botlink').style.opacity = '1';
+        document.getElementById('botlink').style.transform = 'translateY(0)';
+        document.getElementById('botlink').style.transition = 'all 1s ease-out';
+    }, 250); // Delay in milliseconds
+}
