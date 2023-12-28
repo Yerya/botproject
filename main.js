abd60075cb1db0dcb2a8e7620d3b6076f4f77929
Ermolai <ermolay9@gmail.com>
2023-12-28 11:43:56 +0200

    add telegram store links

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ window.onload = function () {
 };
 
 
+document.getElementById('download-telegram-button').addEventListener('click', function() {
+  const userAgent = navigator.userAgent;
+
+  if (/android/i.test(userAgent)) {
+      window.open('https://play.google.com/store/apps/details?id=org.telegram.messenger', '_blank');
+  }
+  else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      window.open('https://apps.apple.com/app/telegram-messenger/id686449807', '_blank');
+  }
+  else {
+      window.open('https://desktop.telegram.org/', '_blank');
+  }
+});
